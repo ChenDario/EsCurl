bd9bc3dd2dea69f237c1fcf3c99d215aa1f4af76
lai2-24 <lai2-24@itismeucci.com>
2024-12-10 13:11:58 +0100

    risposta not found

--- demo/src/main/java/com/example/MyThread.java
+++ demo/src/main/java/com/example/MyThread.java
@@ -27,12 +27,21 @@ public class MyThread extends Thread {
             
             System.out.println("Richiesta Terminata");
 
-            String responseBody  = "<b>Hello World!</b> <img src= \"smile.png\">";
-            out.writeBytes("HTTP/1.1 200 OK\n");
+            /*
+            String responseBody  = "<b>Hello World!</b> <img src = 'smile.png'>";
+            out.writeBytes("HTTP/1.1 200 OK\n");    
             out.writeBytes("Content-Type: text/html\n");
-            out.writeBytes("Content-Length" + responseBody.length() + "\n");
+            out.writeBytes("Content-Length: " + responseBody.length() + "\n");
             out.writeBytes("\n"); //Riga vuota per far capire che ho finito di inviare le intestazioni e inizio col body
-            out.writeBytes(responseBody + "\r\n");
+            out.writeBytes(responseBody + "\r\n"); 
+            */
+            //Risposta di pagina non trovata
+            //String responseBody  = "Pagina non trovata";
+            out.writeBytes("HTTP/1.1 404 Not Found\n");
+            out.writeBytes("Content-Length: 0\n");
+            out.writeBytes("\n"); //Riga vuota per far capire che ho finito di inviare le intestazioni e inizio col body
+            //out.writeBytes(responseBody + "\r\n"); 
+            
 
         } catch (IOException e) { 
             // TODO Auto-generated catch block
