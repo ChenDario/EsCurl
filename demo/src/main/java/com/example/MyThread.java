1b95227f4fa5be5583f4c7a566c3dafd61386f80
lai2-24 <lai2-24@itismeucci.com>
2024-12-10 13:24:45 +0100

    risposta http in 3 casistiche

--- demo/src/main/java/com/example/MyThread.java
+++ demo/src/main/java/com/example/MyThread.java
@@ -20,28 +20,28 @@ public class MyThread extends Thread {
             BufferedReader  in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
 
-            do {      
-                rcv = in.readLine();
-                System.out.println(rcv + "\n");
-            } while (!rcv.equals("") );
+            rcv = in.readLine();
+            System.out.println(rcv + "\n");
+
+            String request[] = rcv.split(" ");
+
+            String page = request[1].replaceFirst("^/", "");
             
             System.out.println("Richiesta Terminata");
 
-            /*
-            String responseBody  = "<b>Hello World!</b> <img src = 'smile.png'>";
-            out.writeBytes("HTTP/1.1 200 OK\n");    
-            out.writeBytes("Content-Type: text/html\n");
-            out.writeBytes("Content-Length: " + responseBody.length() + "\n");
-            out.writeBytes("\n"); //Riga vuota per far capire che ho finito di inviare le intestazioni e inizio col body
-            out.writeBytes(responseBody + "\r\n"); 
-            */
-            //Risposta di pagina non trovata
-            //String responseBody  = "Pagina non trovata";
-            out.writeBytes("HTTP/1.1 404 Not Found\n");
-            out.writeBytes("Content-Length: 0\n");
-            out.writeBytes("\n"); //Riga vuota per far capire che ho finito di inviare le intestazioni e inizio col body
-            //out.writeBytes(responseBody + "\r\n"); 
-            
+            //Risposta
+            if(page.isBlank() || page.equals("index.html") || page.equals("file.txt")){
+                String responseBody  = "<b><i>Benvenuti nella WebPage di Chen!</i></b>";
+                out.writeBytes("HTTP/1.1 200 OK\n");
+                out.writeBytes("Content-Type: text/html\n");
+                out.writeBytes("Content-Length: " + responseBody.length() + "\n");
+                out.writeBytes("\n"); //Riga vuota per far capire che ho finito di inviare le intestazioni e inizio col body
+                out.writeBytes(responseBody + "\r\n"); 
+            } else {
+                out.writeBytes("HTTP/1.1 404 Not Found\n");
+                out.writeBytes("Content-Length: 0\n");
+                out.writeBytes("\n");
+            }
 
         } catch (IOException e) { 
             // TODO Auto-generated catch block
