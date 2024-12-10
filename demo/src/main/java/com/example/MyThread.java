0634e503cdb7c693694b724217eee985909d2a63
lai2-24 <lai2-24@itismeucci.com>
2024-12-10 14:03:00 +0100

    risposta tramite file su htdocs

--- demo/src/main/java/com/example/MyThread.java
+++ demo/src/main/java/com/example/MyThread.java
@@ -1,9 +1,6 @@
 package com.example;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 
 public class MyThread extends Thread {
@@ -15,38 +12,55 @@ public class MyThread extends Thread {
     }
 
     public void run() {
-       
-        try {
-            BufferedReader  in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+        do {
+            try {
+                BufferedReader  in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                DataOutputStream out = new DataOutputStream(s.getOutputStream());
+    
+                rcv = in.readLine();
+                System.out.println(rcv + "\n");
+    
+                String request[] = rcv.split(" ");
+    
+                String page = request[1].replaceFirst("^/", "");
+                
+                System.out.println("Richiesta Terminata");
+    
+                //Risposta
+                if(page.isBlank() || page.equals("index.html") || page.equals("file.txt")){
+                    //String responseBody  = "<h1><b><i><center>Benvenuti nella WebPage di Chen!<center></i></b></h1>";
+                    
+                    //Accedo al file
+                    File file = new File("htdocs/index.html");
+                    InputStream input = new FileInputStream(file);
 
-            rcv = in.readLine();
-            System.out.println(rcv + "\n");
+                    out.writeBytes("HTTP/1.1 200 OK\n");
+                    out.writeBytes("Content-Length: " + file.length() + "\n");
+                    out.writeBytes("Content-Type: text/html\n");
+                    //out.writeBytes("Content-Length: " + responseBody.length() + "\n");
+                    out.writeBytes("\n"); //Riga vuota per far capire che ho finito di inviare le intestazioni e inizio col body
+                    //out.writeBytes(responseBody + "\r\n"); 
 
-            String request[] = rcv.split(" ");
+                    //Invio il file
+                    byte[] buf = new byte[8192];
+                    int n;
+                    while((n = input.read(buf)) != -1){
+                        out.write(buf, 0, n);
+                    }
+                    input.close();
 
-            String page = request[1].replaceFirst("^/", "");
-            
-            System.out.println("Richiesta Terminata");
-
-            //Risposta
-            if(page.isBlank() || page.equals("index.html") || page.equals("file.txt")){
-                String responseBody  = "<h1><b><i><center>Benvenuti nella WebPage di Chen!<center></i></b></h1>";
-                out.writeBytes("HTTP/1.1 200 OK\n");
-                out.writeBytes("Content-Type: text/html\n");
-                out.writeBytes("Content-Length: " + responseBody.length() + "\n");
-                out.writeBytes("\n"); //Riga vuota per far capire che ho finito di inviare le intestazioni e inizio col body
-                out.writeBytes(responseBody + "\r\n"); 
-
-            } else {
-                out.writeBytes("HTTP/1.1 404 Not Found\n");
-                out.writeBytes("Content-Length: 0\n");
-                out.writeBytes("\n");
-            }
-
-        } catch (IOException e) { 
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }   
+    
+                } else {
+                    out.writeBytes("HTTP/1.1 404 Not Found\n");
+                    out.writeBytes("Content-Length: 0\n");
+                    out.writeBytes("\n");
+                }
+    
+            } catch (IOException e) { 
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }    
+        } while (true);
+           
     }
 }
